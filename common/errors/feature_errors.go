4e201b66739c276017913951e8bd9dfc53d9bb59
sgzhan <sgzhan@users.noreply.github.com>
2026-01-24 17:28:56 +0800

    Comment out LogWarning in PrintNonRemovalDeprecatedFeatureWarning
    Commented out the LogWarning call in PrintNonRemovalDeprecatedFeatureWarning function.

--- common/errors/feature_errors.go
+++ common/errors/feature_errors.go
@@ -7,7 +7,8 @@ import (
 // PrintNonRemovalDeprecatedFeatureWarning prints a warning of the deprecated feature that won't be removed in the near future.
 // Do not remove this function even there is no reference to it.
 func PrintNonRemovalDeprecatedFeatureWarning(sourceFeature string, targetFeature string) {
-	LogWarning(context.Background(), "The feature "+sourceFeature+" is deprecated, not recommended for using and might be removed. Please migrate to "+targetFeature+" as soon as possible.")
+	// LogWarning(context.Background(), "The feature "+sourceFeature+" is deprecated, not recommended for using and might be removed. Please migrate to "+targetFeature+" as soon as possible.")
+	return
 }
 
 // PrintDeprecatedFeatureWarning prints a warning for deprecated and going to be removed feature.
